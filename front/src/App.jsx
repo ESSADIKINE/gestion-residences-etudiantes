df7e8fbbbe6891f4a7a9b855300b420b0f5ed630
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2024-12-17 11:08:40 +0100

    Initialisation du Front utilisant React

---
+++ front/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Application de Gestion de Résidences Étudiantes</h1>
+    </div>
+  );
+}
+
+export default App;
