62b06b7c3ef7c4f10829afa00446242480cdfcaf
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2024-12-18 18:56:48 +0100

    Fix the ussue of extention files

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
     return (
