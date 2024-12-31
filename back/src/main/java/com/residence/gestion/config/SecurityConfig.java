41f956f8720f6f4b470abf90816fd978589a13ed
[200~essadikine.fst@uhp.ac.ma <essadikine.fst@uhp.ac.ma>
2024-12-31 23:42:43 +0100

    clean code

--- back/src/main/java/com/residence/gestion/config/SecurityConfig.java
+++ back/src/main/java/com/residence/gestion/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.residence.gestion.config;
 
-import com.residence.gestion.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
